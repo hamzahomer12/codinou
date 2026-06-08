95a43fce8675f03c12f9bfb0e7b9db24b1085ee0
Dumbeard <hamzahomer12@gmail.com>
2026-06-08 16:55:40 +0100

    fix api

--- middleware.ts
+++ middleware.ts
@@ -23,8 +23,8 @@ export async function middleware(request: NextRequest) {
     response = NextResponse.rewrite(url)
   }
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?.trim()
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim()
 
   if (supabaseUrl && supabaseAnonKey) {
     const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
